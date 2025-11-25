76c9e7729c5e1693a3ddebb8dcb2218007aa2865
thebao200474 <truongnguyenthebao742004@gmail.com>
2025-11-25 17:52:02 +0700

    Delete canbang.php

--- canbang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require __DIR__ . '/bootstrap.php';
-
-$_SESSION['flash_message'] = 'Trang cân bằng phương trình đã được gỡ. Bạn có thể ôn luyện bằng các đề thi mới.';
-header('Location: ' . app_url());
-exit;
