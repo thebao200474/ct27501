aeef3e4d4d92f3022de988f4ad17bbb4e11f96ef
thebao200474 <truongnguyenthebao742004@gmail.com>
2025-11-25 17:52:13 +0700

    Delete cauhoi.php

--- cauhoi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require __DIR__ . '/bootstrap.php';
-
-$_SESSION['flash_message'] = 'Trang làm câu hỏi đã được gỡ. Vui lòng luyện đề tại mục Đề thi.';
-header('Location: ' . app_url());
-exit;
